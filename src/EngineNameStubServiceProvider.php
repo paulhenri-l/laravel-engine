905d2a6880259ba3e6e8af1f422e98054d277d1f
paulhenri-l <25308170+paulhenri-l@users.noreply.github.com>
2020-06-14 02:44:27 +0200

    Package discovery and comments

--- src/EngineNameStubServiceProvider.php
+++ src/EngineNameStubServiceProvider.php
@@ -6,11 +6,17 @@ use PaulhenriL\LaravelEngineCore\EngineServiceProvider;
 
 class EngineNameStubServiceProvider extends EngineServiceProvider
 {
+    /**
+     * Register any application services.
+     */
     public function register()
     {
         //
     }
 
+    /**
+     * Bootstrap any application services.
+     */
     public function boot()
     {
         //
